01cb008a74f0b38b48e8a9169fd293211b85b575
Muhammadreza Haghiri <haghiri75@jabirproject.org>
2015-04-19 00:05:35 +0430

    Script is now better

--- rasooldisk-modify.rb
+++ rasooldisk-modify.rb
@@ -3,9 +3,9 @@ require 'highline/import'
 
 password = ask("Enter your password: "){|c| c.echo = "*"}
 
-cmd = system "echo #{password} | sudo -S mount /dev/sr1 /media"
+cmd = system "echo #{password} | sudo -S mount /dev/sr1 /media/dvdrw"
 
-while cmd != true
+while (Dir.entries('/media/dvdrw') - %w{. ..}).empty? == true
     system 'eject /dev/sr1'
     system 'sleep 10'
     system 'eject -t /dev/sr1'
