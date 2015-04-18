59aa41cdc061516cd8c02f9f4c23d5b65872db55
Muhammadreza Haghiri <haghiri75@jabirproject.org>
2015-04-19 00:21:43 +0430

    Script is now better

--- rasooldisk-modify.rb
+++ rasooldisk-modify.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'highline/import'
 
+print 'Enter device number: '
+dev = gets.chomp
 password = ask("Enter your password: "){|c| c.echo = "*"}
 
 
@@ -8,8 +10,8 @@ password = ask("Enter your password: "){|c| c.echo = "*"}
 while (Dir.entries('/media/dvdrw') - %w{. ..}).empty? == true
     system 'eject /dev/sr1'
     system 'sleep 10'
-    system "echo #{password} | sudo -S mount /dev/sr1 /media/dvdrw"
-    system 'sleep 15'
     system 'eject -t /dev/sr1'
     system 'sleep 20'
+    system "echo #{password} | sudo -S mount /dev/sr#{dev} /media/dvdrw"
+    system 'sleep 15'
 end
