fad3df7fcac1c61b2055666666c210fc41547ff5
Muhammadreza Haghiri <haghiri75@gmail.com>
2017-01-11 20:51:38 +0330

    Some problems solved after a long long time ! :))))))))))))

--- rasooldisk-modify.rb
+++ rasooldisk-modify.rb
@@ -8,9 +8,9 @@ password = ask("Enter your password: "){|c| c.echo = "*"}
 
 
 while (Dir.entries('/media/dvdrw') - %w{. ..}).empty? == true
-    system 'eject /dev/sr#{dev}'
+    system "eject /dev/sr#{dev}"
     system 'sleep 10'
-    system 'eject -t /dev/sr#{dev}'
+    system "eject -t /dev/sr#{dev}"
     system 'sleep 20'
     system "echo #{password} | sudo -S mount /dev/sr#{dev} /media/dvdrw"
     system 'sleep 15'
